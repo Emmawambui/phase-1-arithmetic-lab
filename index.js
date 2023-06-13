c0eb3d4794d841d62381f2ce8d4dfd5c879c3798
emma wambui <emma.wambui@student.moringaschool.com>
2023-06-13 11:48:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let multiply, num1 =31, num2=2;
+multiply = (num1*num2);
 
-// Write your code here
+let random 
+random = Math.round(Math.random() * 10);
+
+
+let mod, num3 =40, num4=12;
+mod = (num3%num4);
+
+let max = Math.max(15,16,17,18,19,20);
